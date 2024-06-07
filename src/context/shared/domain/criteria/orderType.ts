6b66586680bbe54afb898c0e15973c5a855e8c26
Juan Salamanca <jpsalamanca99@gmail.com>
2024-06-06 20:22:09 -0500

    Finish OrderType class

--- src/context/shared/domain/criteria/orderType.ts
+++ src/context/shared/domain/criteria/orderType.ts
@@ -1,5 +1,32 @@
-export enum OrderType {
+import { EnumValueObject } from '../valueObject/enum.ValueObject';
+import { InvalidArgumentError } from '../error/invalidArgument.error';
+
+export enum OrderTypes {
   ASC = 'asc',
   DESC = 'desc',
   NONE = 'none'
 }
+
+export class OrderType extends EnumValueObject<OrderTypes> {
+  constructor(value: OrderTypes) {
+    super(value, Object.values(OrderTypes));
+  }
+
+  public static fromValue(value: string): OrderType {
+    const orderValues = Object.values(OrderTypes) as string[];
+    if (orderValues.includes(value)) {
+      return new OrderType(value as OrderTypes);
+    } else {
+      throw new InvalidArgumentError(`The order type ${value} is invalid`);
+    }
+  }
+  
+  public isNone(): boolean {
+    return this.value === OrderTypes.NONE;
+  }
+  
+  protected throwErrorForInvalidValue(value: OrderTypes): void {
+    throw new InvalidArgumentError(`The order type ${value} is invalid`);
+  }
+}
+
