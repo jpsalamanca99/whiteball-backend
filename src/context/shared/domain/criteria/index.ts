4040292fea2b4a34b30e8823a747ad761b673688
Juan Salamanca <jpsalamanca99@gmail.com>
2024-06-24 10:18:06 -0500

    Add criteria index

---
+++ src/context/shared/domain/criteria/index.ts
@@ -0,0 +1,22 @@
+import { Criteria } from './criteria';
+import { Filter } from './filter';
+import { Filters } from './filters';
+import { FilterField } from './filterField';
+import { FilterOperator, Operator } from './filterOperator';
+import { FilterValue } from './filterValue';
+import { Order } from './order';
+import { OrderBy } from './orderBy';
+import { OrderTypes } from './orderType';
+
+export {
+  Criteria,
+  Filter,
+  Filters,
+  FilterField,
+  FilterOperator,
+  FilterValue,
+  Order,
+  OrderBy,
+  OrderTypes,
+  Operator,
+};
